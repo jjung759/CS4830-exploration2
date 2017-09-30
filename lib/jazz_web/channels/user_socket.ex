9b7a7ad9afbce850867a2a41446d9877c6b7387b
Jim <jmjhw9@mail.missouri.edu>
2017-09-29 21:48:26 -0500

    fixing things

--- lib/jazz_web/channels/user_socket.ex
+++ lib/jazz_web/channels/user_socket.ex
@@ -5,8 +5,8 @@ defmodule JazzWeb.UserSocket do
   # channel "room:*", JazzWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
-  timeout: 45_000
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
@@ -24,15 +24,5 @@ defmodule JazzWeb.UserSocket do
     {:ok, socket}
   end
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
-  #
-  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
-  #
-  # Would allow you to broadcast a "disconnect" event and terminate
-  # all active sockets and channels for a given user:
-  #
-  #     JazzWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
-  #
-  # Returning `nil` makes this socket anonymous.
   def id(_socket), do: nil
 end
